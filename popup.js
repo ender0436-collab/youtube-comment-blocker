738d1b5e4c80a3d369b12afc388c383297bde685
ender0436-collab <ender0436@gmail.com>
2026-09-07 03:05:13 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,4 @@
-document.addEventListener("DOMContentLoaded", async () => {
-
-    const data = await chrome.storage.sync.get({
-        hiddenCount: 0
-    });
-
-    document.getElementById("counter").textContent =
-        `非表示件数: ${data.hiddenCount}`;
+document.addEventListener("DOMContentLoaded", () => {
 
     document
         .getElementById("openOptions")
