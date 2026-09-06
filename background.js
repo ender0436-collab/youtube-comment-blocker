664800c71cf28dd62238e597d7567f1f3afee80a
ender0436-collab <ender0436@gmail.com>
2026-09-07 03:05:40 +0900

    Update background.js

--- background.js
+++ background.js
@@ -2,8 +2,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
     chrome.storage.sync.set({
         blockedUsers: [],
-        blockedWords: [],
-        hiddenCount: 0
+        blockedWords: []
     });
 
 });
